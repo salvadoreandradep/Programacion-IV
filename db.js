11f1a4de25edd5128b9718c56b3938ece9fd85ae
Salvadoreandradep <salvadorplay7089@gmail.com.com>
2024-02-13 09:01:52 -0600

    Clase

--- db.js
+++ db.js
@@ -1,31 +1,26 @@
 var db;
-
-const funedb = () => {
-    let indexDB = indexedDB.open("db_sistem", 1);
-    
-    indexDB.onupgradeneeded = e => {
-        let db = e.target.result,
-            tblproducto = db.createObjectStore("producto", { keyPath: 'idproducto' });
-        tblproducto.createIndex('idproducto', 'idproducto', { unique: true });
-        tblproducto.createIndex('codigo', 'codigo', { unique: true });
+const funcdb = ()=>{
+    let indexDB = indexedDB.open('db_sistema',1);
+    indexDB.onupgradeneeded = e=>{
+        let req = e.target.result,
+            tblproducto = req.createObjectStore('productos',{keyPath:'idProducto'}),
+            tblcategoria = req.createObjectStore('categorias',{keyPath:'idCategoria'}),
+            tblcliente = req.createObjectStore('clientes',{keyPath:'idCliente'});
+        tblproducto.createIndex('idProducto','idProducto',{unique:true});
+        tblproducto.createIndex('codigo','codigo',{unique:true});
+        tblcategoria.createIndex('idCategoria','idCategoria',{unique:true});
+        tblcategoria.createIndex('codigo','codigo',{unique:true});
+        tblcliente.createIndex('idCliente','idCliente',{unique:true});
+        tblcliente.createIndex('codigo','codigo',{unique:true});
     };
-
-    indexDB.onsuccess = e => {
+    indexDB.onsuccess = e=>{
         db = e.target.result;
     };
-
-    indexDB.onerror = e => {
-        console.error("Error en IndexedDB: ", e.target.error.message);
+    indexDB.onerror = e=>{
+        console.error('Error al crear la base de datos', e.message());
     };
+}, abrirStore = (store, modo)=>{
+    let ltx = db.transaction(store, modo);
+    return ltx.objectStore(store);
 };
-
-const abrirStore = (store, modo) => {
-    if (!db) {
-        console.error("La base de datos no está inicializada");
-        return;
-    }
-    let transaction = db.transaction(store, modo);
-    return transaction.objectStore(store);
-};
-
-funedb();
+funcdb();
