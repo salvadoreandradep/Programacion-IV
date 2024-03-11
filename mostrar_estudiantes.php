b47f0c8917aa0ed451a5174e8363a7da3f564517
salvadoreandradep <salvadorplay7089@gmail.com>
2024-03-11 09:34:50 -0600

    CRUD estudiante

--- mostrar_estudiantes.php
+++ mostrar_estudiantes.php
@@ -23,12 +23,48 @@
             display: flex;
             justify-content: space-between;
         }
+        nav {
+            background-color: #333;
+            color: #fff;
+            text-align: center;
+            padding: 10px 0;
+        }
+        nav ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+        }
+        nav ul li {
+            display: inline;
+            margin-right: 20px;
+        }
+        nav ul li a {
+            color: #fff;
+            text-decoration: none;
+        }
+        nav ul li a:hover {
+            text-decoration: underline;
+        }
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 0;
+        }
     </style>
 </head>
 <body>
-
+<nav>
+        <ul>
+            <li><a href="inicio.html">Inicio</a></li>
+            <li><a href="estudiante.html">Estudiantes</a></li>
+            <li><a href="#materia">Materia</a></li>
+            <li><a href="#matricula">Matrícula</a></li>
+            <li><a href="#inscripcion">Inscripción</a></li>
+        </ul>
+    </nav>
+<center> 
 <h2>Listado de Estudiantes</h2>
-
+    </center>
 <table>
     <thead>
         <tr>
