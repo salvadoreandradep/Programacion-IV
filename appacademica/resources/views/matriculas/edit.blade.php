448800f38f382c942519e20e5c61048ba8e61ff6
salvadoreandradep <salvadorplay7089@gmail.com>
2024-04-06 10:22:57 -0600

    CRUD - estudiantes - Materia - Matricula

--- appacademica/resources/views/matriculas/edit.blade.php
+++ appacademica/resources/views/matriculas/edit.blade.php
@@ -4,7 +4,25 @@
     <label>Código:</label>
     <input type="text" name="codigo" value="{{ $matricula->codigo }}"><br>
     <label>Estudiante:</label>
-    <input type="text" name="estudiante_id" value="{{ $matricula->estudiante_id }}"><br>
+    <select name="estudiante_id">
+        <?php
+        $conexion = new mysqli("localhost", "root", "", "laravel");
+        if ($conexion->connect_error) {
+            die("Error de conexión: " . $conexion->connect_error);
+        }
+
+        $query = "SELECT id, nombre FROM estudiantes";
+        $result = $conexion->query($query);
+
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value='" . $row['id'] . "'>" . $row['nombre'] . "</option>";
+        }
+
+        $conexion->close();
+        ?>
+    </select><br>
+
+
     <label>Ciclo:</label>
     <input type="text" name="ciclo" value="{{ $matricula->ciclo }}"><br>
     <label>Fecha de Matrícula:</label>
