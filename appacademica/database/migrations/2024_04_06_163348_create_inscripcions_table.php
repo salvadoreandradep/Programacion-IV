7937adde6920ff3d480b46e50f3dd811fc310641
salvadoreandradep <salvadorplay7089@gmail.com>
2024-04-06 10:57:32 -0600

    Agregar inscripciones

--- appacademica/database/migrations/2024_04_06_163348_create_inscripcions_table.php
+++ appacademica/database/migrations/2024_04_06_163348_create_inscripcions_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('inscripciones', function (Blueprint $table) {
+        Schema::create('inscripcions', function (Blueprint $table) {
             $table->id();
             $table->foreignId('estudiante_id')->constrained()->onDelete('cascade');
             $table->foreignId('materia_id')->constrained()->onDelete('cascade');
@@ -30,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('inscripciones');
+        Schema::dropIfExists('inscripcions');
     }
 };
