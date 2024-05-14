c3554e5440b71257d60d7ba129eee04165364654
salvadoreandradep <salvadorplay7089@gmail.com>
2024-05-14 00:20:21 -0600

    Diseño full

--- Casos/ver_detalle_caso.php
+++ Casos/ver_detalle_caso.php
@@ -60,9 +60,144 @@ if (isset($_GET['logout'])) {
             height: 500px;
             border: none;
         }
+
+
+        
+:root {
+  --main-color: #242975; /* Cambio de color principal */
+  --accent-color: #2D6653; /* Nuevo color de acento */
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+
+
+
+.main-header {
+  background: var(--main-color); /* Usar el color principal */
+  width: 100%;
+  height: 50px;
+  display: flex; /* Alinear el contenido del encabezado */
+  align-items: center; /* Alinear verticalmente */
+  justify-content: space-between; /* Espacio entre los elementos */
+  padding: 0 20px; /* Agregar un poco de espacio alrededor del contenido */
+}
+
+nav {
+  position: absolute;
+  left: 0;
+  top: 50px;
+  width: 200px;
+  height: calc(100vh - 50px);
+  background: var(--accent-color); /* Usar el nuevo color de acento */
+  transform: translateX(-100%);
+  transition: .4s ease;
+  background-color: #E6F0FF;
+}
+
+.navigation li {
+  list-style: none;
+  width: 100%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+
+  
+}
+
+.navigation a {
+  color: #242975; /* Cambiar el color del texto a blanco */
+  background-color: #E6F0FF;
+  display: block;
+  line-height: 3.5;
+  padding: 15px 20px; /* Aumentar el espacio alrededor del texto */
+  text-decoration: none;
+  transition: .4s ease;
+  font-family: Bahnschrift;
+}
+
+.navigation a:hover {
+  background-color: #242975; /* Agregar un color de fondo al pasar el cursor */
+  color: #E6F0FF;
+  font-family: Bahnschrift;
+}
+
+#btn-nav {
+  display: none;
+}
+
+#btn-nav:checked ~ nav {
+  transform: translateX(0);
+}
+
+.btn-nav {
+  color: #fff; /* Cambiar el color del botón a blanco */
+  font-size: 20px; /* Reducir un poco el tamaño del botón */
+  cursor: pointer;
+  padding: 10px 15px; /* Ajustar el espacio alrededor del botón */
+  transition: .2s ease;
+  background: transparent; /* Hacer el botón transparente */
+  border: none; /* Eliminar el borde del botón */
+  outline: none; /* Eliminar el contorno del botón al hacer clic */
+}
+
+.btn-nav:hover {
+  background: rgba(255, 255, 255, 0.1); /* Cambiar el color de fondo al pasar el cursor */
+}
+
+.circle-container {
+        width: 70px;
+        height: 70px;
+        border-radius: 50%; /* Esto hace que el borde sea redondeado, creando un círculo */
+        overflow: hidden; /* Oculta cualquier contenido fuera del círculo */
+        margin: 50px; /* Añade un margen de 10px alrededor del círculo */
+        border: 2px solid #ccc; /* Agrega un borde para mayor claridad */
+    }
+    
+    /* Estilo para la imagen */
+    .circle-image {
+        width: 100%; /* Ajusta el ancho de la imagen al 100% del contenedor */
+        height: auto; /* Mantiene la proporción de la imagen */
+    }
+    h1{
+      color: W;
+      font-size: 10px;
+      font-family: Bahnschrift;
+    }
+    h2{
+      color: white;
+      font-size: 20px;
+      font-family: Bahnschrift;
+    }
     </style>
 </head>
 <body>
+
+<header class="main-header">
+
+    <label for="btn-nav" class="btn-nav">&#9776;</label>
+    <input type="checkbox" id="btn-nav">
+    <h2>LegalConnect</h2>
+    <nav>
+      <ul class="navigation">
+<center>
+<a href="/Formularios/Perfil.php">
+<div class="circle-container">
+
+    <img class="circle-image" src="recursos/profile.png" alt="Tu imagen">
+
+   </div>
+  </a>
+        <li><a href="#">Inicio</a></li>
+        <li><a href="/Audiencias/Buscar_Audiencias.php">Audiencias</a></li>
+        <li><a href="/Casos/Buscar_Casos.php">Casos</a></li>
+        <li><a href="?logout">Cerrar Sesion</a></li>
+        <h1>LegalConnect v.1</h1>
+      </ul>
+    </nav>
+    </center>
+  </header>
 
 
 
